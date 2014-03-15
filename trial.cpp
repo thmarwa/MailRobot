0b2df55d21082674ee79598e08a3f4786faa4c60
thmarwa <thmarwa@gmail.com>
2014-03-15 19:57:28 -0400

    Create trial.cpp

---
+++ trial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int thabet = 4;
+  
+  cout << "Thabet is worth $" << thabet << endl;
+
+  // This line is useless. Just put it for fun.
+  return 0;
+
+}
