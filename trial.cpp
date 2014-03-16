67ed2e906cfa40bdcaca5a3288e0d488215ccfaf
thmarwa <thmarwa@gmail.com>
2014-03-15 20:05:40 -0400

    Fixed a small bug regarding not pausing at end of program

--- trial.cpp
+++ trial.cpp
@@ -5,7 +5,13 @@ int main() {
   
   int thabet = 4;
   
-  cout << "Thabet is worth $" << thabet << endl;
+  cout << "Thabet is worth $" << thabet << endl << endl;
+  
+
+// These lines allow the code to pause at the end
+  cout << "Press return to continue..." << endl;
+  cin.get();
+  cin.get();
 
   // This line is useless. Just put it for fun.
   return 0;
